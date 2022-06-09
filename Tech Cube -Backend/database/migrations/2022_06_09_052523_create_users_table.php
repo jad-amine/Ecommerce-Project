818c4939fce017b6920eed4e3647b9da1965c775
jad amine <jade1992amine@gmail.com>
2022-06-09 08:26:45 +0300

    Created users table and migrated to db

--- Tech Cube -Backend/database/migrations/2022_06_09_052523_create_users_table.php
+++ Tech Cube -Backend/database/migrations/2022_06_09_052523_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,8 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('JWT-Token');
+            $table->integer('type');
+            $table->json('wishlist');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -33,4 +35,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
