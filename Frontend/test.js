bcfba3ed03025f623432c9c79439aab2dd6c97b2
jad amine <jade1992amine@gmail.com>
2022-06-11 09:34:22 +0300

    Added LogIn functionality

--- Frontend/test.js
+++ Frontend/test.js
@@ -1,38 +1,32 @@
 const like = document.querySelector("#like");
 const login = document.querySelector("#login");
 const item = document.querySelector("#item");
+const logout = document.querySelector("#logout");
 
-let data = new FormData();
+var data = new FormData();
 data.append('email', 'jad@sef.com');
 data.append('password', 'test1234');
 
 // Like
 like.addEventListener("click", ()=>{
-  axios.post('http://localhost:8000/api/like', data,
+  axios.post('http://localhost:8000/api/like','',
     { headers: { Authorization: "Bearer" + `${localStorage.getItem('token')}`}})
   .then(res => console.log(res.data));
 })
 
 
-//Login 
-login.addEventListener("click", ()=>{
-  let data = new FormData();
-  data.append('email', 'jad@sef.com');
-  data.append('password', 'test1234');
-  
-  axios.post('http://localhost:8000/api/login', data)
-  .then(res => {
-    console.log(res.data)
-    data = res.data;
-    token = data.authorisation.token;
-    localStorage.setItem('token', token);
-    console.log(localStorage);
-  })
-});
+
 
 
 item.addEventListener("click", ()=>{
-  axios.post('http://localhost:8000/api/item', data,
+  axios.post('http://localhost:8000/api/item', '',
     { headers: { Authorization: "Bearer" + `${localStorage.getItem('token')}`}})
   .then(res => console.log(res.data));
+})
+
+
+logout.addEventListener("click", ()=>{
+  axios.post('http://localhost:8000/api/logout', '',
+  { headers: { Authorization: "Bearer" + `${localStorage.getItem('token')}`}})
+  .then(res=>localStorage.clear());
 })
