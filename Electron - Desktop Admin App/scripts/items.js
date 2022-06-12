1b8ac362f7e87b287259a5deb06633ebabf4c692
jad amine <jade1992amine@gmail.com>
2022-06-12 18:55:30 +0300

    Added Delete item functionality

--- Electron - Desktop Admin App/scripts/items.js
+++ Electron - Desktop Admin App/scripts/items.js
@@ -11,33 +11,25 @@ axios.get("http://localhost:8000/api/items",
     let cell4 = row.insertCell(3);
     let cell5 = row.insertCell(4);
     let cell6 = row.insertCell(5);
-    let cell7 = row.insertCell(6);
     cell1.innerHTML = item.id;
     cell2.innerHTML = item.name;
     cell3.innerHTML = item.price;
     cell4.innerHTML = item.offer;
     cell5.innerHTML = item.image;
-    cell6.innerHTML = "Update";
-    cell6.classList.add("greyer");
-    // cell6.style.backgroundColor = "rgb(171, 171, 171)";
-    cell7.innerHTML = "Delete";
-    cell7.classList.add("reder");
-    // cell7.style.backgroundColor = "red";
+    cell6.innerHTML = "Delete";
+    cell6.classList.add("reder");
     cell6.addEventListener("click", ()=>{
-      
+      axios.delete(`http://localhost:8000/api/item/${item.id}`, {
+        headers: {
+          Authorization: `bearer ${localStorage.getItem("token")}`
+        }
+      })
+      .then(res => location.reload())
+      .catch(err => console.log(err));
     })
-    cell7.addEventListener("click", ()=>{
-      
-    })
-    
-    // let icon = document.createElement("i");
-    // icon.classList.add = "fa-solid";
-    // icon.classList.add = "fa-pen";
-    // document.appendchild(icon);
-    // cell7.innerHTML = 'hello';
   }
 })
-// .catch(err=>{
-//   alert("You are not an Admin");
-//   location.replace("../index.html");
-// });
+.catch(err=>{
+  alert("You are not an Admin");
+  location.replace("../index.html");
+});
