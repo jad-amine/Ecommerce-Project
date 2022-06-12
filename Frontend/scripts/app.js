267b03fa452f8698a94382111720567c1a7ba6fd
jad amine <jade1992amine@gmail.com>
2022-06-12 13:26:10 +0300

    Added Likes Section

--- Frontend/scripts/app.js
+++ Frontend/scripts/app.js
@@ -11,31 +11,67 @@ if(localStorage.getItem('name')){
 } 
 
 
-axios.get("http://localhost:8000/api/getLikes",
-  {headers: { Authorization : `Bearer ${localStorage.getItem('token')}`}})
-  .then(res => {
-    console.log(res.data, 'likes');
-  });
-
+  // Populate offer section
 axios.get("http://localhost:8000/api/offer")
   .then(res => {
     const offers = document.querySelector(".offers");
     let offer = res.data.items;
     offer.forEach(offer => {
+      // console.log(offer);
       var div = document.createElement('div');
-      div.innerHTML = `${offer.name}`
-      let icon = document.createElement("i");
-      icon.addEventListener("click", ()=>{
-        console.log("liked");
-        icon.style.color = "red";
-      })
-      icon.classList.add("fa-solid");
-      icon.classList.add("fa-heart");
-      div.appendChild(icon);
+      div.innerHTML = ` Name: ${offer.name} <br> ID: ${offer.id}`
+      // let icon = document.createElement("i");
+      // icon.addEventListener("click", ()=>{
+      //   axios.post(`http://localhost:8000/api/like/ ${offer.id}`,'',
+      //   { headers: { Authorization: "Bearer" + `${localStorage.getItem('token')}`}})
+      //   .then(res => console.log(res.data));
+      //   icon.classList.toggle("red");
+      // })
+      // icon.classList.add("fa-solid");
+      // icon.classList.add("red");
+      // icon.classList.add("fa-heart");
+      // icon.style.cursor = "pointer";
+      // div.appendChild(icon);
       offers.appendChild(div)
     });
   });
 
+  // Populate Likes Section
+axios.get("http://localhost:8000/api/getLikes",
+{headers: { Authorization : `Bearer ${localStorage.getItem('token')}`}})
+.then(res => {
+  const like_section = document.querySelector(".likes");
+  if(res.data.status == 'error'){
+    like_section.innerHTML = "Please Login to add Items"
+    return
+  };
+  if(res.data.message == 'No likes'){
+    like_section.innerHTML = "You haven't Like Anything So Far !"
+    return;
+  };
+  let likes = res.data.result;
+  for (let i=0; i<likes.length; i++){
+    console.log(likes[i])
+    var div = document.createElement('div');
+    div.innerHTML = ` Name: ${likes[i].name} <br> ID: ${likes[i].id}`
+    let icon = document.createElement("i");
+    icon.addEventListener("click", ()=>{
+      axios.post(`http://localhost:8000/api/like/ ${likes[i].id}`,'',
+      { headers: { Authorization: "Bearer" + `${localStorage.getItem('token')}`}})
+      .then(res => console.log(res.data));
+      icon.classList.toggle("red");
+    })
+    icon.classList.add("fa-solid");
+    icon.classList.add("red");
+    icon.classList.add("fa-heart");
+    icon.style.cursor = "pointer";
+    div.appendChild(icon);
+    like_section.appendChild(div)
+  }
+})
+
+  
+  
 
 
 
