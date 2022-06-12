eed4cd5fd60254325d30747137bbd696315c3b84
jad amine <jade1992amine@gmail.com>
2022-06-12 05:54:37 +0300

    Added electron desktop app

--- Frontend/test.js
+++ Frontend/test.js
@@ -2,6 +2,29 @@ const like = document.querySelector("#like");
 const login = document.querySelector("#login");
 const item = document.querySelector("#item");
 const logout = document.querySelector("#logout");
+const file = document.querySelector("#file");
+
+file.addEventListener("change", ()=>{
+  var fileSelected = file.files;
+  var fileToLoad = fileSelected[0];
+  var fileReader = new FileReader();
+  var base64;
+  fileReader.onload = function (fileLoadedEvent){
+    base64 = fileLoadedEvent.target.result;
+    console.log(base64);
+    base64 = base64.split(",")[1];
+    console.log(base64);
+  }
+  fileReader.readAsDataURL(fileToLoad);
+  let data = new FormData();
+  data.append('img', JSON.stringify(base64));
+
+  axios.post('http://localhost:8000/base64.php',data).then(res=>console.log(res.data));
+})
+
+
+
+
 
 var data = new FormData();
 data.append('email', 'jad@sef.com');
@@ -13,10 +36,6 @@ like.addEventListener("click", ()=>{
     { headers: { Authorization: "Bearer" + `${localStorage.getItem('token')}`}})
   .then(res => console.log(res.data));
 })
-
-
-
-
 
 item.addEventListener("click", ()=>{
   axios.post('http://localhost:8000/api/item', '',
