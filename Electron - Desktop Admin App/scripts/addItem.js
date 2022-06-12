63c939f9e35b8885e2a38f02d3685020a9fbf9bb
jad amine <jade1992amine@gmail.com>
2022-06-12 08:28:27 +0300

    Added addItem functionality

---
+++ Electron - Desktop Admin App/scripts/addItem.js
@@ -0,0 +1,27 @@
+// Grab elements
+const name1 = document.querySelector("#name");
+const price = document.querySelector("#price");
+const offer = document.querySelector("#offer");
+const image = document.querySelector("#image");
+const add_item = document.querySelector("#add-item");
+
+add_item.addEventListener("click", postData);
+
+function postData(){
+  console.log('hi');
+  if(!name1.value || !price.value || !offer.value){
+    alert('Please fill all the fields');
+    return;
+  } 
+  let data = new FormData();
+  data.append('name', name1.value);
+  data.append('price', price.value);
+  data.append('offer', offer.value);
+  data.append('image', image.value);
+
+  axios.post("http://localhost:8000/api/item", data, {
+    headers: {
+      'Authorization': `bearer ${localStorage.getItem('token')}`
+    }
+  }).then(res => console.log(res.data));
+}
